26d0c300ebf36f9e2c69f4bc5fe4ed8a25992394
RYadav40 <Rahul.Yadav40@T-Mobile.com>
2024-03-26 16:24:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+interface ServerElement {
+  price: number;
+  name: string;
+  description: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'component-demo';
+  serverElements: ServerElement[] = [];
+  productName: string = '';
+  productDescription: string = '';
+  productPrice: number = 0;
+
+  onAddproduct() {
+    this.serverElements.push({
+      name: this.productName,
+      description: this.productDescription,
+      price: this.productPrice
+    });
+  }
+
 }
